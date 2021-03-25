2345de60fef63a463f3c45a8eda235738e9748c1
Rodolfo Della Violla <rodolfo.violla@eduzz.com>
2021-03-25 16:11:33 -0300

    Finishes all challenges

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,17 +14,22 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder('games')
+      .where('LOWER(title) LIKE LOWER(:title)', { title: `%${param}%` })
+      .getMany();
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT COUNT(id) as count FROM games');
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const usersRepository = getRepository(User);
+    
+    return usersRepository
+      .createQueryBuilder('users')
+      .leftJoinAndSelect('users.games', 'games')
+      .where('games.id = :id', { id })
+      .getMany();
   }
 }
